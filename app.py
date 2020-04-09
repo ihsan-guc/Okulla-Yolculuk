d992d5bf3abd52d18f9e4ece83928bf5a68ffcc4
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-04-10 01:29:31 +0300

    Okulla Yolculuk

---
+++ app.py
@@ -0,0 +1,33 @@
+﻿#import the libraries
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+#flask configuration
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+#route configurations
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+#prediction route
+@app.route("/predict", methods = ['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    predictionValue = str(prediction)
+    print(str(prediction))
+    if str(prediction) == "[[0. 0. 1.]]":
+        output = "Yürüyerek ";
+    if str(prediction) == "[[0. 1. 0.]]":
+        output = "Bisiklet ile";    
+    if str(prediction) == "[[1. 0. 0.]]":
+        output = "Araba ile ";
+    return render_template("index2.html", prediction_text = "Girdiğiniz verilere göre  {} Gidersiniz".format(output));
+
+#run the app
+if __name__ == "__main__":
+    app.run(debug = True)
